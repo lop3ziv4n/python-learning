a099c4b39e2a58f0b9b4d192c40e816720400366
Ivan Leandro Lopez <ilopez@fleni.com.ar>
2019-08-19 16:59:57 -0300

    orm peewee

---
+++ orm/orm_peewee.py
@@ -0,0 +1,32 @@
+import datetime
+
+import peewee
+
+database = peewee.MySQLDatabase('peewee_test', host='localhost', port=3306, user='root', password='root')
+
+
+#
+class User(peewee.Model):
+    username = peewee.CharField(unique=True)
+    email = peewee.CharField(index=True)
+    created_date = peewee.DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = database
+        db_table = 'User'
+
+
+if __name__ == '__main__':
+    if not User.table_exists():
+        User.create_table()
+
+    username = input("Input Username")
+    email = input("Input Email")
+
+    # Select
+    if not User.select().where(User.username == username).exists():
+        # Insert
+        new_user = User.create(username=username, email=email)
+        new_user.save()
+    else:
+        print('User exists')
